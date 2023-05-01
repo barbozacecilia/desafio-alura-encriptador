10329b8e2f94b7eb90a8d5ddf9062650e2bfaa79
Cecilia <barboza.ceciliam@gmail.com>
2023-05-01 17:31:52 -0300

    feat(Button): WIP, working on feature

---
+++ JavaScript/main.js
@@ -0,0 +1,13 @@
+function buttonEncrypt() {
+    document.getElementById("encrypted-container").style.display = "none";
+    document.getElementById("encrypt").style.display="flex";
+}
+
+function buttonDecrypt() {
+    document.getElementById("encrypted-container").style.display = "flex";
+    document.getElementById("encrypt").style.display="none";
+}
+
+function buttonCopy(){
+    
+}
